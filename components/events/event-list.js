a3824a6d8148c1b5c664196d406df6df55ea54f4
laurentravix <laurentravix@gmail.com>
2022-02-21 09:15:02 +0100

    First commit

---
+++ components/events/event-list.js
@@ -0,0 +1,14 @@
+import EventItem from "./event-item";
+import styles from "./event-list.module.css"
+
+const EventList = ({ items }) => {
+  return (
+    <ul className={styles.list}>
+      {items.map((item) => (
+        <EventItem key={item.id} id={item.id} title={item.title} location={item.location} date={item.date} image={item.image}/>
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
