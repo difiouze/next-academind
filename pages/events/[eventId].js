8a5ab8b06e2c9a143a02705e233d73a2fe837073
laurentravix <laurentravix@gmail.com>
2022-02-23 21:56:57 +0100

    event Id

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,19 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
 const EventDetailPage = () => {
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+    
+    if (!event) {
+        return <p>No event found!</p>
+    }
+
     return (
         <div>
             <h1>Event Detail</h1>
+            {event.description}
         </div>
     )
 }
